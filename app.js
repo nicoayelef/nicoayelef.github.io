e89e57ff1fbe4ab8775cd024f6f3c15c434160a2
nicoayelef <nicolas.ayelef@gmail.com>
2025-02-28 20:18:27 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+// Configuración de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBYaNbZWHUS-Pvm49kmMtHw9LqqxUDySYA",
+  authDomain: "base-de-datos-poli.firebaseapp.com",
+  projectId: "base-de-datos-poli",
+  storageBucket: "base-de-datos-poli.appspot.com", // Corregido el storageBucket
+  messagingSenderId: "954754202697",
+  appId: "1:954754202697:web:e06171f6b0ade314259398"
+};
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Inicializar Firestore
+const db = firebase.firestore();
+
 // Variables globales
 let currentPatientId = null;
 let currentEvolutionId = null;
